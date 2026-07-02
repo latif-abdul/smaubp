319039d0ad88c130fb806c3c050893ac4ed24f65
latif-abdul <54927430+latif-abdul@users.noreply.github.com>
2026-07-02 15:14:03 +0700

    Update DafulExport.php

--- app/Exports/DafulExport.php
+++ app/Exports/DafulExport.php
@@ -14,11 +14,27 @@ class DafulExport implements FromCollection, WithHeadings
     */
     public function collection()
     {
-        return Santris::join('daful', 'daful.id_santris', '=', 'santris.id')->distinct()->get(['no_pendaftaran', 'nama_lengkap']);
+        return Santris::leftJoin('daful', 'daful.id_santris', '=', 'santris.id')
+			->leftJoin('batch', 'santris.batch_id', '=', 'batch.id')
+			->leftJoin('tahun_ajaran', 'batch.tahun_ajaran_id', '=', 'tahun_ajaran.id')
+			->where('daful.id_santris', '!=', null)
+			->where('santris.deleted_at', '=', null)
+			->where('batch.deleted_at', '=', null)
+			->where('tahun_ajaran.deleted_at', '=', null)
+			->where('daful.deleted_at', '=', null)
+            ->distinct()->get('santris.*')
+            ->makeHidden(['created_at','updated_at', 'deleted_at', 'batch_id','sertifikat','id','foto','bukti_pembayaran','status']);
     }
 
     public function headings(): array
     {
-        return ["No Pendaftaran", "Nama"];
+        return ['Email','Nama Lengkap','Jenis Kelamin','Tempat Lahir','Tanggal Lahir',
+            'Asal Sekolah','Tahun Lulus','Alamat Sekolah','Nama Ayah','Nama Ibu',
+            'No HP Ayah','No HP Ibu','Pekerjaan Ayah','Pekerjaan Ibu',
+            'Penghasilan Ayah','Penghasilan Ibu','Jalur Masuk',
+            'No Pendaftaran','No Whatsapp','Status Lulus','NIK','NISN','Anak Ke-','Jumlah Saudara',
+            'Tinggi Badan','Berat Badan','Nomor KK','Tempat Tanggal Lahir Ayah','NIK Ayah','Tempat Tanggal Lahir Ibu',
+            'NIK Ibu','Alamat Orang Tua','Dusun','Desa','Kecamatan','Kabupaten Kota','Provinsi',
+            'Alamat'];
     }
 }
